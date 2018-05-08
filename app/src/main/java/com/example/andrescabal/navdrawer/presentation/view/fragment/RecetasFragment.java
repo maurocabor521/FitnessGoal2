984dfe0cae5168598d4c30e4ac3e81a00f32b159
maurocabor521 <amacor521@gmail.com>
2018-05-08 12:44:10 -0500

    1commit

--- app/src/main/java/com/example/andrescabal/navdrawer/presentation/view/fragment/RecetasFragment.java
+++ app/src/main/java/com/example/andrescabal/navdrawer/presentation/view/fragment/RecetasFragment.java
@@ -7,11 +7,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.andrescabal.navdrawer.R;
+import com.example.andrescabal.navdrawer.domain.model.Alimento;
 import com.example.andrescabal.navdrawer.presentation.contract.CategoriaAlimentosContract;
 import com.example.andrescabal.navdrawer.presentation.presenter.CategoriaAlimentosPresenter;
 import com.example.andrescabal.navdrawer.presentation.view.activity.CategoriaAlimentosActivity;
@@ -25,25 +29,20 @@ import java.util.List;
  * Activities that contain this fragment must implement the
  * {@link RecetasFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link RecetasFragment#newInstance} factory method to
- * create an instance of this fragment.
+
  */
-public class RecetasFragment extends Fragment implements CategoriaAlimentosContract.View {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class RecetasFragment extends Fragment implements CategoriaAlimentosContract.View,View.OnClickListener {
+
 
     private CategoriaAlimentosContract.Presenter presenter;
     private SpinnerAdapter spinnerAdapter;
     private List<String> lstNombresAlimentos;
     private Spinner spinner;
     CategoriaAlimentosActivity categoriaAlimentosActivity;
+    private Button btnAgregarAlimento;
+    private EditText etListaAlim;
 
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -51,32 +50,7 @@ public class RecetasFragment extends Fragment implements CategoriaAlimentosContr
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecetasFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecetasFragment newInstance(String param1, String param2) {
-        RecetasFragment fragment = new RecetasFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -86,6 +60,9 @@ public class RecetasFragment extends Fragment implements CategoriaAlimentosContr
 
 
         View view = inflater.inflate(R.layout.fragment_recetas, container, false);
+        btnAgregarAlimento=view.findViewById(R.id.btIngresarAlimento);
+        btnAgregarAlimento.setOnClickListener(this);
+        etListaAlim=view.findViewById(R.id.etListaAlimentos);
         //presenter=new C
 
 
@@ -94,10 +71,10 @@ public class RecetasFragment extends Fragment implements CategoriaAlimentosContr
         /*lstNombresAlimentos.add("casa");
         lstNombresAlimentos.add("carro");
         lstNombresAlimentos.add("moto");*/
-        spinner=(Spinner)view.findViewById(R.id.spinner);
-       presenter=new CategoriaAlimentosPresenter(this);
-       presenter.loadListaAlimentos();
-        Toast.makeText(getContext(),"on create presenter fragment",Toast.LENGTH_SHORT).show();
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+        presenter = new CategoriaAlimentosPresenter(this);
+        presenter.loadListaAlimentos();
+        Toast.makeText(getContext(), "on create presenter fragment", Toast.LENGTH_SHORT).show();
 
         return view;
     }
@@ -133,33 +110,44 @@ public class RecetasFragment extends Fragment implements CategoriaAlimentosContr
 
     @Override
     public void showListaAlimentos() {
-        lstNombresAlimentos=presenter.getLstNombreAlimentos();
-        int sizeList=lstNombresAlimentos.size();
+        lstNombresAlimentos = presenter.getLstNombreAlimentos();
+        int sizeList = lstNombresAlimentos.size();
 
-        if(lstNombresAlimentos!=null){
-            Toast.makeText(getContext(),"lista con nombre "+sizeList,Toast.LENGTH_SHORT).show();
+        if (lstNombresAlimentos != null) {
+            Toast.makeText(getContext(), "lista con nombre " + sizeList, Toast.LENGTH_SHORT).show();
         }
-        Context context;
-        //if (view != null) {
-            //categoriaAlimentosActivity=new CategoriaAlimentosActivity();
-            //context=categoriaAlimentosActivity.getContext();
-            context=getContext();
+        //Context context;
+
+        //context = getContext();
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                Alimento alimento = presenter.getListaAlimentos().get(position);
+                etListaAlim.setText("Nombre: "+ String.valueOf(alimento.getNameAlimento()));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, lstNombresAlimentos);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btIngresarAlimento:
+                //etListaAlim.setText();
+                break;
+        }
+    }
+
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
